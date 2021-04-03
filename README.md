82f14a222d43d9caa7a9f8e5a4a953b7ae05e955
mooreFL <moorepr09@gmail.com>
2021-04-03 15:41:50 -0400

    changed css styling, added comments in html and script

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Work-Day-Scheduler
+
+A hourly work day (9-5) calender that changes stylistically by hour based on actual time using momentjs. 
+
+Yellow = Past Hours
+Pink= Current Hour
+Green= Future Hours
+
+The application saves the users text input in hour blocks and can be recalled if the user saves, leaves then reloads the page. 
+
+The page uses jQuery to run a function that evaluates the current hour(using momentjs) to the hour blocks on the calender, this function then informs the css styling to display one of the three colors.
+
+Initial opening of the application at 2:00pm:
+<img src="assets\imgs\Initial loading.PNG" alt="Hourly calender with 9 rows representing 9 workday hours">
+
+User filled form:
+<img src="assets\imgs\Filled Form.PNG" alt="same calender as above with personal activities in each hours row">
+
+
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,16 @@ $(document).ready(function () {
   $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
   //
   $(".saveBtn").on("click", function () {
+    //holds what was typed
+    var text = $(this).siblings(".description").val(); 
+    //gets the id of the parent
+    var time = $(this).parent().attr("id"); 
     console.log(this);
-    var text = $(this).siblings(".description").val(); //
-    var time = $(this).parent().attr("id"); //
-
     // save in local storage
     localStorage.setItem(time, text);
   });
-  //load any saved data from LocalStorage - do this for every hour
+
+  //load any saved data from LocalStorage, do this for every hour
   $("#hour-9 .description").val(localStorage.getItem("hour-9"));
   $("#hour-10 .description").val(localStorage.getItem("hour-10"));
   $("#hour-11 .description").val(localStorage.getItem("hour-11"));
@@ -21,10 +23,9 @@ $(document).ready(function () {
   $("#hour-16 .description").val(localStorage.getItem("hour-16"));
   $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-  function timeEquiv() {
-    //get the current hour using moment, disregard the minutes in the format by using "HH"
-    var currentHour = moment("4/3/2021 12:00").format("HH");
-
+  function timeEval() {
+    //get the current hour using moment, disregard the minutes in the format by using "HH" and not :mm
+    var currentHour = moment().format("HH");
     // calls for dialogue class and returns each of the dialogues
     $(".dialogue").each(function () {
         //grabs the id of the dialogues and removes the "hour-" to return only the integer. 
@@ -43,7 +44,7 @@ $(document).ready(function () {
       }
     });
   }
-  timeEquiv(); //
+  timeEval(); 
 });
 
 
